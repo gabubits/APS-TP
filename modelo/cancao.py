f85e53a47efe094283f89d316f493f7f3c666003
Gabriel Gomes <gomesg827@gmail.com>
2024-09-04 22:19:53 -0300

    Modelo atualizado

--- modelo/cancao.py
+++ modelo/cancao.py
@@ -1,30 +1,20 @@
 from modelo.entidade import (dataclass, Entidade)
-from typing import List, Dict
-from datetime import datetime
-from dataclasses import field
+from typing import Dict
 
 @dataclass
 class Cancao(Entidade):
     titulo: str
     genero: str
-    album: int
+    album: str
     diretorio_audio: str
-    data_adicao: datetime
-    artistas: List[int] = field(default_factory=list)
-
-    def add_artista(self, artista_id: int) -> None:
-        self.artistas.append(artista_id)
-
-    def rem_artista(self, artista_id: int) -> None:
-        self.artistas.remove(artista_id)
+    artista: str
     
     @staticmethod
     def from_dict(cancao_dict: Dict):
         id = cancao_dict["id"]
         titulo = cancao_dict["titulo"]
-        artistas = cancao_dict["artistas"]
+        artista = cancao_dict["artista"]
         genero = cancao_dict["genero"]
         album = cancao_dict["album"]
         diretorio_audio = cancao_dict["diretorio_audio"]
-        data_adicao = datetime.fromisoformat(cancao_dict["data_edicao"])
-        return Cancao(id, titulo, artistas, genero, album, diretorio_audio, data_adicao)
+        return Cancao(id, titulo, genero, album, diretorio_audio, artista)
--- modelo/playlist.py
+++ modelo/playlist.py
@@ -1,5 +1,5 @@
 from modelo.entidade import (dataclass, Entidade)
-from datetime import datetime
+from modelo.cancao import Cancao
 from typing import List, Dict
 from dataclasses import field
 
@@ -8,21 +8,18 @@ class Playlist(Entidade):
     nome: str
     descricao: str
     img_capa: str
-    data_criacao: datetime
-    cancoes: List[int] = field(default_factory=list)
+    cancoes: List[Cancao] = field(default_factory=list)
 
-    def add_cancao(self, cancao_id: int) -> None:
-        self.cancoes.append(cancao_id)
+    def add_cancao(self, cancao: Cancao) -> None:
+        self.cancoes.append(cancao)
     
-    def rem_cancao(self, cancao_id: int) -> None:
-        self.cancoes.remove(cancao_id)
+    def rem_cancao(self, cancao: Cancao) -> None:
+        self.cancoes.remove(cancao)
     
     @staticmethod
-    def from_dict(playlist_dict: Dict):
+    def from_dict(playlist_dict: Dict, cancoes: List[Cancao]):
         id = playlist_dict["id"]
         nome = playlist_dict["nome"]
         descricao = playlist_dict["descricao"]
         img_capa = playlist_dict["img_capa"]
-        data_criacao = datetime.fromisoformat(playlist_dict["data_criacao"])
-        cancoes = playlist_dict["cancoes"]
-        return Playlist(id, nome, descricao, img_capa, data_criacao, cancoes)
+        return Playlist(id, nome, descricao, img_capa, cancoes)
