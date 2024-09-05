5aed4062bfefe2c0c9e374b30c48ae83ae9cd23e
Gabriel Gomes <gomesg827@gmail.com>
2024-09-05 10:47:43 -0300

    Atualização na tela de tocar canção

--- visao/tela_tocando.py
+++ visao/tela_tocando.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import (
-    Qt, QEvent, QObject
+    Qt, QEvent, QObject, QSize
 )
 from PySide6.QtWidgets import (
     QWidget, QListWidget, QGridLayout,
@@ -7,7 +7,7 @@ from PySide6.QtWidgets import (
     QMessageBox, QLabel, QSlider
 )
 from PySide6.QtGui import (
-    QFont, QPixmap
+    QFont, QPixmap, QIcon
 )
 
 from typing import Dict
@@ -45,19 +45,49 @@ class TelaTocando(QWidget):
         self.img_capa.setScaledContents(True)
         self.img_capa.setPixmap(QPixmap("visao/imgs/album_padrao.jpg"))
 
-        self.botao_tocar = QPushButton("Play")
+        self.botao_tocar = QPushButton()
         self.botao_tocar.setCursor(Qt.CursorShape.PointingHandCursor)
+        img_play = QPixmap("visao/imgs/play.png")
+        icon_play = QIcon(img_play)
+        self.botao_tocar.setIcon(icon_play)
+        self.botao_tocar.setIconSize(QSize(25, 25))
         self.botao_tocar.setFixedSize(100,35)
         self.botao_tocar.setFont(fontes["botao"])
-        self.botao_tocar.setStyleSheet("background-color: rgba(255,255,255,0.9); color: black; border-radius: 10px;")
+        self.botao_tocar.setStyleSheet("background-color: rgba(255,255,255,0.9); color: black; border-radius: 15px;")
+
+        self.botao_anterior = QPushButton()
+        self.botao_anterior.setCursor(Qt.CursorShape.PointingHandCursor)
+        img_anterior = QPixmap("visao/imgs/anterior.png")
+        icon_anterior = QIcon(img_anterior)
+        self.botao_anterior.setIcon(icon_anterior)
+        self.botao_anterior.setIconSize(QSize(25, 25))
+        self.botao_anterior.setFixedSize(100,35)
+        self.botao_anterior.setFont(fontes["botao"])
+        self.botao_anterior.setStyleSheet("background-color: rgba(255,255,255,0.9); color: black; border-radius: 15px;")
+
+        self.botao_proximo = QPushButton()
+        self.botao_proximo.setCursor(Qt.CursorShape.PointingHandCursor)
+        img_proximo = QPixmap("visao/imgs/proximo.png")
+        icon_proximo = QIcon(img_proximo)
+        self.botao_proximo.setIcon(icon_proximo)
+        self.botao_proximo.setIconSize(QSize(25, 25))
+        self.botao_proximo.setFixedSize(100,35)
+        self.botao_proximo.setFont(fontes["botao"])
+        self.botao_proximo.setStyleSheet("background-color: rgba(255,255,255,0.9); color: black; border-radius: 15px;")
 
         self.slider = QSlider(Qt.Orientation.Horizontal)
         self.slider.setFixedSize(100, 20)
+        """QSlider::groove::horizontal{
+            border: 1px solid #999999;
+            height: 2px;}\nQSlider::handle::horizontal{
+            background: #2a2a2a; width: 10px; margin: -5px -1px; border-radius: 5px; border: 1px solid #2a2a2a;}"""
 
         layout_botoes = QGridLayout()
-        layout_botoes.addWidget(self.botao_tocar, 0, 0, Qt.AlignmentFlag.AlignTop)
+        layout_botoes.addWidget(self.botao_anterior, 0, 0, Qt.AlignmentFlag.AlignTop)
+        layout_botoes.addWidget(self.botao_tocar, 0, 1, Qt.AlignmentFlag.AlignTop)
+        layout_botoes.addWidget(self.botao_proximo, 0, 2, Qt.AlignmentFlag.AlignTop)
 
         layout.addLayout(layout_topo, 0, 0, Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.img_capa, 1, 0, Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.slider, 2, 0, Qt.AlignmentFlag.AlignCenter)
-        layout.addLayout(layout_botoes, 2, 0, Qt.AlignmentFlag.AlignCenter)
+        layout.addLayout(layout_botoes, 3, 0, Qt.AlignmentFlag.AlignCenter)
